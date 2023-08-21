1f874c60a4155a522beb9c0c03bab05bd5c8195b
Joshua French <joshua.french@ucdenver.edu>
2023-08-21 18:40:02 +0000

    version 1.3.6

--- R/confreg.R
+++ R/confreg.R
@@ -23,6 +23,18 @@
 #' \item{complement}{The complement of the confidence region.}
 #' 
 #' @importFrom matrixStats colProds
+#' @references 
+#' Joshua P. French and Stephan R. Sain (2013).
+#' Spatio-temporal exceedance locations and confidence
+#' regions.  Annals of Applied Statistics.  7(3):1421-1449.
+#'
+#' French, J. P. (2014), Confidence regions for the level
+#' curves of spatial data, Environmetrics, 25, pages
+#' 498–512, DOI: 10.1002/env.2295
+#'
+#' French, J. P., and Hoeting, J. A. (2016) Credible regions
+#' for exceedance sets of geostatistical data.
+#' Environmetrics, 27: 4–14. doi: 10.1002/env.2371.
 #' @author Joshua French
 #' @export
 #' @examples 
